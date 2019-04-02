381d17630e43382faac4dc0a7b94a2a0291ff21b
ruben120 <44381674+ruben120@users.noreply.github.com>
2019-04-02 14:42:39 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+/ ** 
+ * La clase HelloWorldApp implementa una aplicación que simplemente imprime "Hello World" en la salida estándar.
+ * /
+ clase  pública HelloWorld {
+  public  static  void  main ( String [] args ) {
+    Sistema . a cabo . println ( " Hola, Mundo " );
+  }
+}
